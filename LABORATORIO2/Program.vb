b0bde4c40e8ddde751067c6362b3f8a35fafb3b0
NelsonBV <nelson.barrientos@catolica.edu.sv>
2022-03-04 19:33:45 -0600

    Agregar archivos de proyecto.

---
+++ LABORATORIO2/Program.vb
@@ -0,0 +1,52 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        Console.WriteLine("  Menu  ")
+        Console.WriteLine("(1) SUMA")
+        Console.WriteLine("(2) RESTA")
+        Console.WriteLine("(3) MULTIPLICAR")
+        Console.WriteLine("(4) DIVIDIR")
+        Console.WriteLine("Elige una opcion: ")
+        Console.ReadLine()
+
+
+
+
+        Console.WriteLine("OPERACION DE SUMA")
+        Console.WriteLine("Ingrese valor1: ")
+        Console.ReadLine()
+        Console.WriteLine("ingrese valor2: ")
+        Console.WriteLine()
+        Console.WriteLine("La suma de su operacion es: num1s + num2s")
+
+
+
+        Console.WriteLine("OPERACION DE RESTA")
+        Console.WriteLine("ingrese valor1: ")
+        Console.ReadLine()
+        Console.WriteLine("ingrese valor2: ")
+        Console.ReadLine()
+        Console.WriteLine("la resta de su operacion es: num1r - num2r")
+
+
+        Console.WriteLine("OPERACION DE MULTIPLICAR")
+        Console.WriteLine("ingrese valor1: ")
+        Console.ReadLine()
+        Console.WriteLine("ingrese valor2: ")
+        Console.ReadLine()
+        Console.WriteLine("la multiplicacion de su operacion es: num1m * num2m ")
+
+
+        Console.WriteLine("OPERACION DIVIDIR")
+        Console.WriteLine("ingrese valor1: ")
+        Console.ReadLine()
+        Console.WriteLine("ingrese valor2: ")
+        Console.ReadLine()
+        Console.WriteLine("la multiplicacion de su operacion es: num1d / num2d ")
+
+
+
+    End Sub
+End Module
